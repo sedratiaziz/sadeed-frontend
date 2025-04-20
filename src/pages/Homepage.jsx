8f70b9ac5e33378c5347f2c2bd19f7fddef14fdd
sedratiaziz <87585184+sedratiaziz@users.noreply.github.com>
2025-04-20 17:36:14 +0300

    removed unwanted loading img

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -187,18 +187,7 @@ function Homepage(props) {
         sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
         open={isLoading}
       >
-        <CircularProgress color="inherit" />
-        <Box
-          component="img"
-          sx={{
-            height: 233,
-            width: 350,
-            maxHeight: { xs: 233, md: 167 },
-            maxWidth: { xs: 350, md: 250 },
-          }}
-          alt="The house from the offer."
-          src="https://images.unsplash.com/photo-1512917774080-9991f1c4c750?auto=format&w=350&dpr=2"
-        />
+        <CircularProgress color="inherit" />        
       </Backdrop>
 
       <div className="container">   
